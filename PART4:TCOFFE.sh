0ffce28391cc5e3a51bc7b57318f64ad9d276ee3
lxdeo <lucia.salgado@estudiantat.upc.edu>
2025-04-29 16:55:09 +0200

    Create PART4:TCOFFE.sh

---
+++ PART4:TCOFFE.sh
@@ -0,0 +1,2 @@
+# To conduct the protein sequence aligment, for the obtained protein sequences from both methods, we use the tcoffe server, by running this command in the local terminal:
+$ cat geneid_prot_contig24.fa FGENESH_prot_contig24.fa  homology_protein.fa > comb_pred_protein_contig24.fa
